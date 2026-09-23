2d5fe89fd18adcb8bfeddbc6a7f6d8625af3c38f
KJCJ <kjcjarrett92@outlook.com>
2026-09-23 12:22:46 +0100

    3258852141263772bdc450e22e847032c 586edd4692a56bf4c1d96208b0622aee3

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"af7e796e161ae0bb1ff0758c71a7105418bd9d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4140164614" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4163422261" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
